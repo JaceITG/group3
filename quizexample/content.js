67effd97bc9ff473730c1d1ded9b14701df01dcc
Jace Williams <cs49918@cs.indstate.edu>
2022-09-28 12:38:22 -0400

    his example

---
+++ quizexample/content.js
@@ -0,0 +1,61 @@
+export const questions = [
+    {
+      "id": 1,
+      "question":"What is the capital of Ohio?",
+      "choices":[
+          "Cleveland",
+          "Detroit",
+          "Columbus",
+          "Cincinnati",
+          "Springfield"],
+      "answer": 2
+    }
+    ,
+    {
+      "id": 2,
+      "question":"Which of the following countries borders France?",
+      "choices":[
+          "Poland",
+          "Italy",
+          "England",
+          "Austria",
+          "Australia"],
+      "answer": 1
+    }
+    ,
+    {
+      "id": 3,
+      "question":"What is the longest river in South America?",
+      "choices":[
+          "Mississippi",
+          "Nile",
+          "Congo",
+          "Amazon",
+          "Wabash"],
+      "answer": 3
+    }
+    ,
+    {
+      "id": 4,
+      "question":"Which of the following rivers flows through Paris?",
+      "choices":[
+          "Rhine",
+          "Rhone",
+          "Seine",
+          "Loire",
+          "Garonne"],
+      "answer": 2
+    }
+    ,
+    {
+      "id": 5,
+      "question":"What is the tallest mountain in the USA that is NOT in Alaska?",
+      "choices":[
+          "Mount Whitney",
+          "Pikes Peak",
+          "Mount Rainier",
+          "Mount Elbert",
+          "Mount Wilson"],
+      "answer": 0
+    }
+]
